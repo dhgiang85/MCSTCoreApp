9f1f271df083ddf7a285e3f1eb2a3d7685b1f090
HGIANG <dhgiang85@gmail.com>
2018-05-11 15:54:48 +0700

    add some common components

---
+++ MCSTCoreApp/Controllers/Components/MobileMenuViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MCSTCoreApp.Controllers.Components
+{
+    public class MobileMenuViewComponent : ViewComponent
+    {
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
