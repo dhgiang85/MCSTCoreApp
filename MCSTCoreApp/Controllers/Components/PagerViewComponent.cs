df809a1c9d6d41a7ebfc3484127b66450610be4f
HGIANG <dhgiang85@gmail.com>
2018-05-12 10:52:45 +0700

    Load product catalog

---
+++ MCSTCoreApp/Controllers/Components/PagerViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MCSTCoreApp.Utilities.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MCSTCoreApp.Controllers.Components
+{
+    public class PagerViewComponent : ViewComponent
+    {
+        public Task<IViewComponentResult> InvokeAsync(PagedResultBase result)
+        {
+            return Task.FromResult((IViewComponentResult)View("Default", result));
+        }
+    }
+}
